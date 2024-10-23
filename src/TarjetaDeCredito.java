805ee13537277c1bcc58a058afcce2e3e9847f03
juanse951 <juanse951@gmail.com>
2024-10-23 09:57:39 -0500

    creando las bases, atributos

---
+++ src/TarjetaDeCredito.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TarjetaDeCredito {
+    private double limite;
+    private double saldo;
+    private List<Compra> listaDeCompras;
+
+    public TarjetaDeCredito(double limite) {
+        this.limite = limite;
+        this.saldo = limite;
+        this.listaDeCompras = new ArrayList<>();
+    }
+}
